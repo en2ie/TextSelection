ac6d97c085eb85a1c3d376be9a59c6d0a8d9edcb
Ben Hall <ben.c.hall@gmail.com>
2013-09-12 10:27:56 +0100

    Updated to Air 3.8

--- bat/SetupSDK.bat
+++ bat/SetupSDK.bat
@@ -1,7 +1,7 @@
 :user_configuration
 
 :: Path to Flex SDK
-set FLEX_SDK=C:\Program Files (x86)\FlashDevelop\Tools\flexsdk
+set FLEX_SDK=C:\Program Files (x86)\FlashDevelop\Tools\air3.8sdk
 set AUTO_INSTALL_IOS=yes
 
 :: Path to Android SDK
